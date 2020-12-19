9b97e4dadd74082b6c721e4e2677db84d025b63e
深渊流浪者 <750108747@qq.com>
2020-12-19 22:50:32 +0800

    feat(Renderer): render 接口实现 1. 简单实现render接口,canvas中绘制矩形、文本、圆;

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { defineComponent, h } from "@vue/runtime-core";
 
 export default defineComponent({
     render() {
-        const vnode = h("div");
+        // <rect x=100 y=100>你好,我是文本</rect>
+        const vnode = h("rect", { x: 100, y: 100 }, [
+            "你好,我是文本",
+            h("circle", { x: 150, y: 150 })
+        ]);
         return vnode;
     }
 });
--- src/runtime-canvas/index.js
+++ src/runtime-canvas/index.js
@@ -1,11 +1,35 @@
 import { createRenderer } from "@vue/runtime-core";
+import { Graphics, Text } from "pixi.js";
 
 const { render, createApp } = createRenderer({
     createElement(type) {
-        console.log(type)
+        // 绘制一个矩形 pixi.js
+        let element;
+        if(type === "rect") {
+            element = new Graphics();
+            element.beginFill(0xff0000);
+            element.drawRect(0,0, 500, 500);
+            element.endFill();
+        } else if(type === "circle") {
+            element = new Graphics();
+            element.beginFill(0x00ff00);
+            element.drawCircle(0,0, 100);
+            element.endFill();
+        }
+        return element;
+    },
+    patchProp(el, key, prevVal, nextVal) {
+        if(el) el[key] = nextVal;
+    },
+    setElementText(node, text) {
+        const cText = new Text(text);
+        node.addChild(cText);
+    },
+    createText(text) {
+        return new Text(text);
     },
     insert(el, parent) {
-        console.log(el, parent);
+        if(el) parent.addChild(el);
     }
 });
 export { render, createApp }
