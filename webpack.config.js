a20d639ccab9fe785d5b0ca54f962ca2c35feb5f
深渊流浪者 <750108747@qq.com>
2020-12-19 19:00:43 +0800

    config(webpack): 初始化配置webpack 1. webpack 热更新配置

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const { webpack } = require("webpack");
+
+module.exports = {
+    entry: path.resolve(__dirname, "src/main.js"),
+    output: {
+        filename: "build.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
