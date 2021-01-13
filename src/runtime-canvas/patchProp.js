7d7e1ab8f8e914e62e3b1ca3339190542f308205
深渊流浪者 <750108747@qq.com>
2021-01-13 19:47:15 +0800

    Jdoc(canvas): 添加注释

--- src/runtime-canvas/patchProp.js
+++ src/runtime-canvas/patchProp.js
@@ -1,5 +1,12 @@
 import { Texture } from "pixi.js";
 
+/**
+ *  元素属性处理
+ * @param {element} el 
+ * @param {string} key 
+ * @param {any} prevVal 
+ * @param {any} nextVal 
+ */
 export const patchProp = (el, key, prevVal, nextVal) => {
     switch(key) {
         case "texture":
